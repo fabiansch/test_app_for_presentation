c3e349dddc1155a997436134d28bfbafa6ddbce4
fabian <stahlschiff@web.de>
2018-04-22 14:14:09 +0200

    add feature specs

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -2,6 +2,7 @@
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
+Dir[Rails.root.join("spec/support/**/*.rb")].each{ |f| require f }
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
@@ -57,4 +58,5 @@ RSpec.configure do |config|
 
   # Include Factory Bot syntax to simplify calls to factories
   config.include FactoryBot::Syntax::Methods
+  config.include LoginMacros
 end
