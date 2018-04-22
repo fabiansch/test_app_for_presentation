8879f735791d8c894af26ee5e62d14f8d3290910
fabian <stahlschiff@web.de>
2018-04-22 12:11:55 +0200

    add model spec structure

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
-RSpec.describe Contact, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+describe Contact do
+  it "is valid with a firstname, lastname and email"
+  it "is invalid without a firstname"
+  # other invalid tests ...
+  describe "filter last name by letter" do
+    context "with matching letters" do
+      it "returns a sorted array of results that match"
+    end
+    context "with non-matching letters" do
+      it "omits results that do not match"
+    end
+  end
+end                             # spec/models/contact_spec.rb
