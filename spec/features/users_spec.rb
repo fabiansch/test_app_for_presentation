8559c1da7f1088aa77d7a4eb2bb095c7bc4b6cef
fabian <stahlschiff@web.de>
2018-04-22 14:14:09 +0200

    add feature spec structure

---
+++ spec/features/users_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+feature 'Usermanagement' do
+  scenario "adds a new user"
+end                                                # spec/features/users_spec.rb
---
+++ spec/support/login_macros.rb
@@ -0,0 +1,11 @@
+module LoginMacros
+
+  def sign_in admin
+    visit root_path
+    click_link 'Log In'
+    fill_in 'Email', with: admin.email
+    fill_in 'Password', with: admin.password
+    click_button 'Log In'
+  end
+
+end
