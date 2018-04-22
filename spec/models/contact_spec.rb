503f7ce6d69e8e7d8d37ab9e0cbf8a48285f7065
fabian <stahlschiff@web.de>
2018-04-22 12:11:55 +0200

    add model specs for contact

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,5 +1,41 @@
 require 'rails_helper'
 
-RSpec.describe Contact, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+describe Contact do
+  it "is valid with a firstname, lastname and email" do
+    contact = Contact.new(
+      firstname: 'Aaron',
+      lastname: 'Sumner',
+      email: 'tester@example.com')
+    expect(contact).to be_valid
+  end
+  it "is invalid without a firstname" do
+    contact = Contact.new(firstname: nil)
+    contact.valid?
+    expect(contact.errors[:firstname]).to \
+      include("can't be blank")
+  end
+  # other invalid tests ...
+  describe "filter last name by letter" do
+    before :each do
+      @smith =   Contact.create( firstname: 'John',
+        lastname: 'Smith',   email: 'jsmith@example.com'
+      )
+      @jones =   Contact.create( firstname: 'Tim',
+        lastname: 'Jones',   email: 'tjones@example.com'
+      )
+      @johnson = Contact.create( firstname: 'John',
+        lastname: 'Johnson', email: 'jjohnson@example.com'
+      )
+    end
+    context "with matching letters" do
+      it "returns a sorted array of results that match" do
+        expect(Contact.by_letter("J")).to eq [@johnson, @jones]
+      end
+    end
+    context "with non-matching letters" do
+      it "omits results that do not match" do
+        expect(Contact.by_letter("J")).not_to include @smith
+      end
+    end
+  end
+end                             # spec/models/contact_spec.rb
