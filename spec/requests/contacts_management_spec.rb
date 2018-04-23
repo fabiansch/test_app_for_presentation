d3d8cf94c1c58328ec8e9cdc72a2977e5c5ed3ae
fabian <stahlschiff@web.de>
2018-04-23 22:34:19 +0200

    refactor request test

--- spec/requests/contacts_management_spec.rb
+++ spec/requests/contacts_management_spec.rb
@@ -4,11 +4,11 @@ describe ContactsController do
   # describe 'GET#index' do
   #   context 'with params[:letter]' do
   #     it "populates an array of contacts starting with the letter"
-  #     it "renders the :index template"
+  #     it "returns with http 200 status"
   #   end
   #   context 'without params[:letter]' do
   #     it "populates an array of all contacts"
-  #     it "renders the :index template"
+  #     it "returns with http 200 status"
   #   end
   # end
 end                                  # spec/requests/contacts_management_spec.rb
